b92f3fba9360becb216756af45142fe36d265308
João Pedro <joaopedro.nc@outlook.com>
2023-10-26 20:52:33 -0300

    ✨ feat (ui): add CTA component

--- src/components/Banner/CTA.tsx
+++ src/components/Banner/CTA.tsx
@@ -3,21 +3,22 @@ import { Button } from '../Button'
 
 export function CTA() {
   return (
-    <div>
-      <span className="font-cursive text-lg text-yellow-400">
-        Está com fome?
+    <>
+      <span className="font-cursive text-3xl text-yellow-400">
+        Are you hungry?
       </span>
-      <h1 className="text-lg uppercase text-white">
-        PEÇA SEU BURGUER DO JOHN AGORA. <br />
-        ENTREGA RÁPIDA!
+      <h1 className="mt-4 text-xl uppercase text-white">
+        Order your John&apos; burguer now!
+        <br />
+        Fast delivery.
       </h1>
-      <div className="mt-3 flex flex-col gap-3 md:flex-row">
+      <div className="mt-4 flex flex-col gap-3 md:flex-row">
         <Button>Fazer pedido online</Button>
         <Button>
           <Image src="/icons/whatsapp.svg" width={16} height={16} alt="" />
           Comprar pelo whatsapp
         </Button>
       </div>
-    </div>
+    </>
   )
 }
