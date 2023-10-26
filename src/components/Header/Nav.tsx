6c5c214995e837e3d061904e6138002aeaa3c4d5
João Pedro <joaopedro.nc@outlook.com>
2023-10-26 20:53:37 -0300

    ✨ feat (ui): add Nav component

--- src/components/Header/Nav.tsx
+++ src/components/Header/Nav.tsx
@@ -6,25 +6,27 @@ import { NAV_LINKS } from '@/utils/nav-links'
 
 export function Nav() {
   return (
-    <nav className="z-40 flex h-full w-full flex-col items-center justify-center gap-8 bg-orange-500 md:static md:h-auto md:w-auto md:flex-row md:gap-0">
-      {NAV_LINKS.map((navLink) => (
-        <li key={navLink.title}>
+    <nav className="z-40 h-full w-full bg-orange-500 md:static md:h-auto md:w-auto ">
+      <ul className="flex flex-col items-center justify-center gap-12 md:flex-row md:gap-0">
+        {NAV_LINKS.map((navLink) => (
+          <li key={navLink.title}>
+            <NavLink
+              icon={navLink.icon}
+              title={navLink.title}
+              href={navLink.href}
+              isFilled={false}
+            />
+          </li>
+        ))}
+        <li>
           <NavLink
-            icon={navLink.icon}
-            title={navLink.title}
-            href={navLink.href}
-            isFilled={false}
+            icon="burguer.svg"
+            title="make your own burguer"
+            href="/custom-burguer"
+            isFilled={true}
           />
         </li>
-      ))}
-      <li>
-        <NavLink
-          icon="burguer.svg"
-          title="make your own burguer"
-          href="/custom-burguer"
-          isFilled={true}
-        />
-      </li>
+      </ul>
     </nav>
   )
 }
