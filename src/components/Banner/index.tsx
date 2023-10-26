5cfff3b06e8f0cc116dd45e5ab043fc9c5f8c983
João Pedro <joaopedro.nc@outlook.com>
2023-10-26 20:53:23 -0300

    ✨ feat (ui): add Banner component

--- src/components/Banner/index.tsx
+++ src/components/Banner/index.tsx
@@ -3,19 +3,21 @@ import { CTA } from './CTA'
 
 export function Banner() {
   return (
-    <div className="relative flex h-[24rem] w-full items-center  md:h-[32rem]">
-      <span className="absolute inset-0 z-30 block h-full w-full bg-black opacity-60" />
+    <section
+      id="home"
+      className="relative flex h-[24rem] w-full items-center  md:h-[32rem]"
+    >
+      <span className="absolute inset-0 z-30 block h-full w-full bg-black opacity-50" />
       <Image
         src="/images/banner-1.jpg"
         fill
-        className="absolute z-10 h-auto max-h-[100%] w-auto max-w-[100%]"
         sizes="100vw"
         alt="hamburguer"
         style={{ objectFit: 'cover' }}
       />
-      <div className="container z-30 h-max">
+      <div className="container z-30 h-max w-max">
         <CTA />
       </div>
-    </div>
+    </section>
   )
 }
